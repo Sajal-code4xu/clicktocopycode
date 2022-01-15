525df7c8671467610d178fdd76e3ddd580104b5d
Sajal Gupta <70812171+sajalgupta19@users.noreply.github.com>
2022-01-16 02:39:14 +0530

    Create Jonny.js

---
+++ Jonny.js
@@ -0,0 +1,14 @@
+<pre ><code id="myInput1">
+
+<!--### YOUR ### CODE ### HERE---->
+
+Hey I am Sajal
+
+<!--- Don't Touch below Code ---->
+
+</code><span style="margin-top:10px"><p id="copied1" style="display:inline";></p></span><div style="text-align: right;" ><i class="fa fa-copy fa-black" title ="Copy" onclick="myfun('copied1');copyFunction('myInput1');"  ></i><span><i title="Run" class="fas pH fa-cog fa-pulse"></i></span></div></pre>
+
+<!--- Don't Touch Above Code--->
+
+<br>
+
